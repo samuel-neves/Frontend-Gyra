fda9bb59fd0bcdafcb72b3b65c4c393735213436
samuel-neves <samuel212584@gmail.com>
2021-08-28 10:23:10 -0300

    fix: test remotion

--- src/modules/ChatRoom/tests/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import InitialForm from '../index';
-
-test('renders learn react link', () => {
-  render(<InitialForm />);
-  const textElement = screen.getByText(/Teste/i);
-  expect(textElement).toBeUndefined();
-});
--- src/modules/InitialForm/tests/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import InitialForm from '../index';
-
-test('renders app title', () => {
-  render(<InitialForm />);
-  const textElement = screen.getByText(/Mensagens/i);
-  expect(textElement).toBeInTheDocument();
-});
